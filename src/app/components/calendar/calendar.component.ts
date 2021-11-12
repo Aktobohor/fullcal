e2a1df3bdcee983d75235e07ad455813fc5871ef
Aktobohor <manuel32@hotmail.it>
2021-11-12 16:34:22 +0100

    Creazione di una variabile RRule per visualizzare tutti gli eventi

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {CalendarOptions, formatDate, FullCalendarComponent} from "@fullcalendar/angular";
-import RRule from "rrule";
+import {ByWeekday, Frequency, Options, RRule, RRuleSet, RRuleStrOptions} from "rrule";
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 const httpOptions = {
@@ -49,11 +49,21 @@ interface StructureDto {
   idRandomTask?: number;
 }
 
+interface StrsRemsDto {
+  creator: string;
+}
+
 interface NewReminderDTO {
   reminderDto: ReminderDto;
   structureDto: StructureDto;
+  strsRemsDto: StrsRemsDto;
 }
 
+interface event{
+  title: string;
+  date: string;
+  color: string;
+}
 
 @Component({
   selector: 'app-calendar',
@@ -65,8 +75,9 @@ export class CalendarComponent implements OnInit {
   @ViewChild('calendar') calendarComponent?: FullCalendarComponent;
   today = "";
   display: boolean = false;
+  eventList?: Array<event>;
   selectedDate?: Date;
-  a?: Array<Structure>;
+  reminderDtoList?: Array<ReminderDto>;
   endInstance?: string;
   date_picked?: Date;
   minDate?: Date;
@@ -83,7 +94,7 @@ export class CalendarComponent implements OnInit {
   constructor(private http: HttpClient) {
     this.unlockElements();
     this.instanceType = [
-      {name: "Giorni", code: "RRule.DAYLY"},
+      {name: "Giorni", code: "RRule.DAILY"},
       {name: "Settimane", code: "RRule.WEEKLY"},
       {name: "Mesi", code: "RRule.MONTHLY"},
       {name: "Anni", code: "RRule.YEARLY"}
@@ -94,10 +105,10 @@ export class CalendarComponent implements OnInit {
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick.bind(this), // bind is important!
-    events: [
+    events: this.eventList/*[
       {title: 'event 1', date: '2021-10-01', color: 'orange'},
       {title: 'event 2', date: '2021-10-06', color: 'lightblue'}
-    ],
+    ]*/,
 
     editable: true,
     selectable: true,
@@ -136,9 +147,134 @@ export class CalendarComponent implements OnInit {
 
     this.http.get<Array<ReminderDto>>("http://localhost:8080/reminders")
       .subscribe(data => {
-        console.log(data)
+        this.reminderDtoList = data;
+        console.log(this.reminderDtoList);
+        this.createRRule();
+
+        this.popolaEventsArray()
+
       });
 
+
+
+  }
+
+  createRRule(){
+
+    let dateStart : Array<any> =  this.reminderDtoList![0].r_dt_start.toString().split(",");
+    let dateUntil : Array<any> =  this.reminderDtoList![0].r_until!.toString().split(",");
+
+    var option = RRule.parseString("");
+    option.dtstart = new Date (dateStart[0],dateStart[1]-1,dateStart[2],dateStart[3],dateStart[4]);
+    option.until = new Date (dateUntil[0],dateUntil[1]-1,dateUntil[2],dateUntil[3],dateUntil[4]);
+    option.interval = this.reminderDtoList![0].r_interval;
+    option.freq = this.getFrequencyFromString();
+
+    var rule1 = new RRule(option);
+    this.eventList = new Array<event>();
+    var dateArray = rule1.all();
+    for (let d of dateArray){
+      let z : event = {title:'event', date:d.toDateString(), color:'orange'}
+      this.eventList?.push(z);
+    }
+    console.log(this.eventList);
+    //console.log(rule1.all());
+
+    /*var rule2 = new RRule();
+    rule2.origOptions.dtstart = new Date(dateStart[0],dateStart[1]-1,dateStart[2],dateStart[3],dateStart[4]);
+    rule2.options.until = new Date(dateUntil[0],dateUntil[1]-1,dateUntil[2],dateUntil[3],dateUntil[4]);
+    rule2.options.interval = this.reminderDtoList![0].r_interval;
+    rule2.options.freq = this.getFrequencyFromString();
+    console.log(rule2.all());*/
+
+  }
+
+  private popolaEventsArray() {
+
+  }
+
+  getFrequencyFromStringtoString() : string{
+    switch(this.reminderDtoList![0].r_freq){
+      case "RRule.DAILY": {
+        return "DAILY";
+      }
+      case "RRule.WEEKLY": {
+        return "WEEKLY";
+      }
+      case "RRule.MONTHLY": {
+        return "MONTHLY";
+      }
+      case "RRule.YEARLY": {
+        return "YEARLY";
+      }
+      default:{
+        return "HOURLY";
+      }
+    }
+  }
+
+  getFrequencyFromString() : Frequency{
+    switch(this.reminderDtoList![0].r_freq){
+      case "RRule.DAILY": {
+        return Frequency.DAILY;
+      }
+      case "RRule.WEEKLY": {
+        return Frequency.WEEKLY;
+      }
+      case "RRule.MONTHLY": {
+        return Frequency.MONTHLY;
+      }
+      case "RRule.YEARLY": {
+        return Frequency.YEARLY;
+      }
+      default:{
+        return Frequency.HOURLY;
+      }
+    }
+  }
+
+  getArrayByWeekday() : ByWeekday[]{
+    let byWeekdayArray : Array<ByWeekday> = [];
+    let a = this.reminderDtoList![0].r_byweekday?  this.reminderDtoList![0].r_byweekday : "";
+    if(a != "") {
+      a = a.replace("[", "");
+      a = a.replace("]", "");
+      let array: Array<String> = [];
+      array = a.split(",");
+      for (let r of array) {
+        switch (r) {
+          case "RRule.MO": {
+            byWeekdayArray.push(RRule.MO);
+            break;
+          }
+          case "RRule.TU": {
+            byWeekdayArray.push(RRule.TU);
+            break;
+          }
+          case "RRule.WE": {
+            byWeekdayArray.push(RRule.WE);
+            break;
+          }
+          case "RRule.TH": {
+            byWeekdayArray.push(RRule.TH);
+            break;
+          }
+          case "RRule.FR": {
+            byWeekdayArray.push(RRule.FR);
+            break;
+          }
+          case "RRule.SA": {
+            byWeekdayArray.push(RRule.SA);
+            break;
+          }
+          case "RRule.SU": {
+            byWeekdayArray.push(RRule.SU);
+            break;
+          }
+        }
+      }
+    }
+    return byWeekdayArray;
   }
 
   showDialog() {
@@ -163,20 +299,24 @@ export class CalendarComponent implements OnInit {
       r_tzid: "local"
     };
 
-
     let structureDto: StructureDto = {
       idQuestionary: 1
     };
 
-    var body: NewReminderDTO = {
-      reminderDto: reminderDto,
-      structureDto: structureDto
+    let strsRemsDto: StrsRemsDto = {
+      creator: "a@b.it"
     };
 
-    this.http.post<any>('http://localhost:8080/reminders', body).subscribe(data => {
+    var body: NewReminderDTO = {
+      reminderDto: reminderDto,
+      structureDto: structureDto,
+      strsRemsDto: strsRemsDto
+    };
 
+    this.http.post<any>('http://localhost:8080/reminders/create', body).subscribe(data => {
       console.log(data);
     });
+
 
   }
 
@@ -240,4 +380,6 @@ export class CalendarComponent implements OnInit {
       this.disableOcc = true;
     }
   }
+
+
 }
